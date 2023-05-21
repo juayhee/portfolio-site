25ae92cc2e838eb5e6d9232f1d580a6e3856846f
juayhee <e0524722@u.nus.edu>
2023-05-21 22:08:46 +0800

    Add prettier

--- src/comp/App/App.tsx
+++ src/comp/App/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import Title from '../Title/Title.tsx';
-import Header from '../Header/Header.tsx';
+import React from "react";
+import Title from "../Title/Title.tsx";
+import Header from "../Header/Header.tsx";
 
-import styles from './App.module.css';
-
+import styles from "./App.module.css";
 
 const App = (): React.ReactElement => {
-    return (
-        <>
-            <div className={styles.root}>
-                <Title name="Tan Juay Hee"
-                       desc="aspiring game developer, storyteller, artist"/>
-            </div>
-            <div><Header Home="#"
-                         About="#"
-                         Experience="#"
-                         Works="#"
-                         Contact="#"/>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.root}>
+        <Title
+          name="Tan Juay Hee"
+          desc="aspiring game developer, storyteller, artist"
+        />
+      </div>
+      <div>
+        <Header Home="#" About="#" Experience="#" Works="#" Contact="#" />
+      </div>
+    </>
+  );
 };
 
-export default App
+export default App;
--- src/comp/Header/Header.tsx
+++ src/comp/Header/Header.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import styles from './Header.module.css';
+import React from "react";
+import styles from "./Header.module.css";
 
 type HeaderProps = {
-    [label: string]: string;
+  [label: string]: string;
 };
 const Header = (props: HeaderProps): React.ReactElement => {
-    const labels = Object.keys(props);
-    const urls = Object.values(props);
+  const labels = Object.keys(props);
+  const urls = Object.values(props);
 
-    const length = labels.length;
-    const rows = [];
+  const length = labels.length;
+  const rows = [];
 
-    for (let i = 0; i < length; i++) {
-        rows.push(<a href={urls[i]}>{labels[i]}</a>);
-    }
+  for (let i = 0; i < length; i++) {
+    rows.push(
+      <a className={styles.a} href={urls[i]}>
+        {labels[i]}
+      </a>
+    );
+  }
 
-    return (
-        <>
-            {rows}
-        </>
-    )
+  return <>{rows}</>;
 };
-
 export default Header;
