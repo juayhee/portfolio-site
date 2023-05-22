578d8275461611a05ac416b86a7f8e27f053852d
juayhee <e0524722@u.nus.edu>
2023-05-22 20:57:50 +0800

    Add svg

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Title from "./Title.tsx";
 import Header from "./Header.tsx";
+import Icon from "./Icon.tsx";
 
 import styles from "../css/App.module.css";
 
@@ -10,12 +11,16 @@ const App = (): React.ReactElement => {
       <div className={styles.header}>
         <Header Home="#" About="#" Experience="#" Works="#" Contact="#" />
       </div>
-      
+
       <div className={styles.title}>
         <Title
           name="Tan Juay Hee"
           desc="aspiring game developer, storyteller, artist"
         />
+      </div>
+
+      <div className={styles.icon}>
+        <Icon />
       </div>
     </>
   );
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import styles from '../css/Icon.module.css'
+import personalIcon from "../resources/icon/icon.svg";
 
-const Icon = () : React.Element => {
-  return ();
-}
+const Icon = (): React.Element => {
+  return (
+    <>
+      {/* This makes the SVG always scale to match the size of its parent*/}
+      <img src={personalIcon} alt="Personal Icon" width="100%" height="100%" />
+    </>
+  );
+};
 
 export default Icon;
