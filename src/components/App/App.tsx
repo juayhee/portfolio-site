a09d76689217edef294547564ff382c5a06cd72a
juayhee <e0524722@u.nus.edu>
2023-05-28 22:10:05 +0800

    Change directory organisation Add support for sass Add imports for Krub and Source Sans 3

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "../Title/Title.tsx";
+
+import styles from "./App.module.sass";
+
+const App = (): React.ReactElement => {
+  return (
+    <>
+      <Title />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,11 @@
+import styles from "./Title.module.sass";
+
+function Title(): React.ReactElement {
+  return (
+    <>
+      <div className={styles.titleContainer}>Title</div>
+    </>
+  );
+}
+
+export default Title;
