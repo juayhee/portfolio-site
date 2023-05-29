319ce22fb88a90026a9ed55b5e6a3348a17ee156
juayhee <e0524722@u.nus.edu>
2023-05-29 16:58:44 +0800

    Implement header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Title from "../Title/Title.tsx";
-
+import Header from "../Header/Header.tsx";
 import styles from "./App.module.sass";
 
+const headerProps = {
+  Home: "#",
+  About: "#",
+  Skills: "#",
+  Works: "#",
+  Contact: "#",
+};
 const App = (): React.ReactElement => {
   return (
     <>
-      <Header />
+      <Header {...headerProps} />
     </>
   );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,41 @@
 import React from "react";
 import styles from "./Header.module.sass";
+import personalLogo from "../../resources/icon/icon.svg";
 
-function Header(): React.ReactElement {}
+type MenuItems = {
+  /* Variable number of page labels and their corresponding URLS */
+  [label: string]: string;
+};
+
+function Header(props: MenuItems): React.ReactElement {
+  /* Separate labels:links key value pairs */
+  const labelArray: string[] = [];
+  const linkArray: string[] = [];
+
+  for (const label in props) {
+    labelArray.push(label);
+    linkArray.push(props[label]);
+  }
+
+  /* Put labels and links into <a> elements and store in array */
+  const headerArray: React.ReactElement[] = [];
+  for (let i = 0; i < labelArray.length; i++) {
+    headerArray.push(
+      <a key={labelArray[i]} href={linkArray[i]}>
+        {labelArray[i]}
+      </a>
+    );
+  }
+
+  return (
+    <>
+      <div className={styles.headerContainer}>
+        <img src={personalLogo} alt="Icon" />
+        <div className={styles.links}>{headerArray}</div>
+        <div className={styles.space}></div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
