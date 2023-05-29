cc2687ae1317a4e50cd6624f3f054c4abebabf2c
juayhee <e0524722@u.nus.edu>
2023-05-29 17:59:54 +0800

    Implement content box

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,7 +2,24 @@ import React from "react";
 import styles from "./Content.module.sass";
 
 function Content(): React.ReactElement {
-  return <></>;
+  return (
+    <>
+      <div className={styles.box}>
+        <div className={styles.contentContainer}>
+          <h1>Content title</h1>
+          <h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h2>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Content;
