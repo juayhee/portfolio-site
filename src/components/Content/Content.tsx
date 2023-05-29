097309a24f240e37c2af23218adcdb4b6442654a
juayhee <e0524722@u.nus.edu>
2023-05-29 17:46:51 +0800

    Setup

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Content.module.sass";
+
+function Content(): React.ReactElement {
+  return <></>;
+}
+
+export default Content;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App.tsx";
+import Content from "./components/Content/Content.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Content />
   </React.StrictMode>
 );
