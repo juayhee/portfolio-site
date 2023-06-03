ecb16caabcbf086f7ed845b59558554f9d511f67
juayhee <e0524722@u.nus.edu>
2023-06-03 13:03:59 +0800

    Add skillshome page

---
+++ src/components/SkillsHome/SkillsHome.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styles from "./SkillsHome.module.sass";
+
+type BubbleProps = {
+  title: string;
+  skillsList: string[];
+  className: string;
+};
+
+// Content
+const frontEnd: BubbleProps = {
+  title: "Frontend",
+  className: "frontEnd",
+  skillsList: ["Javascript", "Typescript", "HTML/CSS"],
+};
+
+const backEnd: BubbleProps = {
+  title: "Backend",
+  className: "backEnd",
+  skillsList: ["Python", "Java", "C++"],
+};
+
+const more: BubbleProps = {
+  title: "...and more",
+  className: "more",
+  skillsList: ["Video editing", "Photo editing", "Storyboarding", "Music"],
+};
+
+// End of content
+
+function Bubble(props: BubbleProps): React.ReactElement {
+  const { title, skillsList, className } = props;
+  return (
+    <>
+      <div className={`${styles.bubbleContainer} ${styles[className]}`}>
+        <div className={styles.bubbleTitle}>{title}</div>
+        <div className={styles.bubbleContent}>
+          <ul>
+            {skillsList.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function SkillsHome(): React.ReactElement {
+  return (
+    <>
+      <div className={styles.bubblesGrid}>
+        <Bubble {...frontEnd} />
+        <Bubble {...backEnd} />
+        <Bubble {...more} />
+      </div>
+    </>
+  );
+}
+
+export default SkillsHome;
