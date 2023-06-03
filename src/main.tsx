db9e0c8d024af67d97f57025a75e03030d7c9bdd
juayhee <e0524722@u.nus.edu>
2023-06-03 17:46:38 +0800

    Reset main render

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App.tsx";
-import Content from "./components/Content/Content.tsx";
+import HomeSkills from "./components/HomeSkills/HomeSkills.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HomeSkills />
   </React.StrictMode>
 );
