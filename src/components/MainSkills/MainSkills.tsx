f5df33ddde2e2ebcfdab5bc8a3307a9ee682cc0d
juayhee <e0524722@u.nus.edu>
2023-06-04 22:34:40 +0800

    Add icon set for frontend skills

--- src/components/MainSkills/MainSkills.tsx
+++ src/components/MainSkills/MainSkills.tsx
@@ -1,35 +1,44 @@
 import React from "react";
 import styles from "./MainSkills.module.sass";
 
+import javaScriptLogo from "../../assets/MainSkills_icons/javascript.svg";
+import typeScriptLogo from "../../assets/MainSkills_icons/typescript.svg";
+import cssLogo from "../../assets/MainSkills_icons/css.svg";
+import htmlLogo from "../../assets/MainSkills_icons/html.svg";
+
 type BubbleProps = {
   title: string;
-  skillsList: string[];
   className: string;
+  skillsList: string[];
+  skillsIconList: string[];
 };
 
 // Content
 const frontEnd: BubbleProps = {
   title: "Frontend",
   className: "frontEnd",
-  skillsList: ["Javascript", "Typescript", "HTML/CSS"],
+  skillsList: ["Javascript", "Typescript", "HTML", "CSS"],
+  skillsIconList: [javaScriptLogo, typeScriptLogo, htmlLogo, cssLogo],
 };
 
 const backEnd: BubbleProps = {
   title: "Backend",
   className: "backEnd",
   skillsList: ["Python", "Java", "C++"],
+  skillsIconList: [],
 };
 
 const more: BubbleProps = {
   title: "...and more",
   className: "more",
   skillsList: ["Video editing", "Photo editing", "Storyboarding", "Music"],
+  skillsIconList: [],
 };
 
 // End of content
 
 function Bubble(props: BubbleProps): React.ReactElement {
-  const { title, skillsList, className } = props;
+  const { title, className, skillsList, skillsIconList } = props;
   return (
     <>
       <div className={`${styles.bubbleContainer} ${styles[className]}`}>
@@ -37,7 +46,10 @@ function Bubble(props: BubbleProps): React.ReactElement {
         <div className={styles.bubbleContent}>
           <ul>
             {skillsList.map((skill, index) => (
-              <li key={index}>{skill}</li>
+              <li key={index}>
+                <img src={skillsIconList[index]} />
+                {skill}
+              </li>
             ))}
           </ul>
         </div>
