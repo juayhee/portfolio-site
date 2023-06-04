e73491ded0a74ca644897f2c8041776526e1ca0e
juayhee <e0524722@u.nus.edu>
2023-06-04 11:01:08 +0800

    reset main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App.tsx";
+import Contact from "./components/Contact/Contact.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Contact />
   </React.StrictMode>
 );
