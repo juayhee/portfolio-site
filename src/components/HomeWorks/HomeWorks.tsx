442a685935d4aa8f4bb9983642fd9ac28ed6f282
juayhee <e0524722@u.nus.edu>
2023-06-17 16:17:13 +0800

    add gallery to home page

---
+++ src/components/HomeWorks/HomeWorks.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./HomeWorks.module.scss";
+import Gallery from "../util/Gallery/Gallery.tsx";
+
+function HomeWorks(): React.ReactElement {
+  return (
+    <>
+      <Gallery />
+    </>
+  );
+}
+
+export default HomeWorks;
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -4,6 +4,7 @@ import styles from "./Home.module.scss";
 import MainSkills from "../../components/MainSkills/MainSkills.tsx";
 import Contact from "../../components/Contact/Contact.tsx";
 import MainContent from "../../components/MainContent/MainContent.tsx";
+import HomeWorks from "../../components/HomeWorks/HomeWorks.tsx";
 
 const Home = (): React.ReactElement => {
   return (
@@ -11,15 +12,19 @@ const Home = (): React.ReactElement => {
       <div className={styles.titleContainer}>
         <Title />
       </div>
-      <div className={styles.contentContainer}>
+      <div className={styles.homeContentContainer}>
         <MainContent />
       </div>
-      <div className={styles.contentSkillsSpacing} />
-      <div className={styles.skillsContainer}>
+      <div className={styles.homeContentSkillsSpacing} />
+      <div className={styles.homeSkillsContainer}>
         <MainSkills />
       </div>
-      <div className={styles.mainSkillsContactSpacing} />
-      <div className={styles.contactContainer}>
+      <div className={styles.homeSkillsContactSpacing} />
+      <div className={styles.homeWorksContainer}>
+        <HomeWorks />
+      </div>
+      <div className={styles.homeWorksContactSpacing} />
+      <div className={styles.homeContactContainer}>
         <Contact />
       </div>
     </>
